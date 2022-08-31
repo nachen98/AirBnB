cadf89a395ec6908400fd105193412c6ec86fd60
nachen98 <nachen98@gmail.com>
2022-08-31 11:43:56 -0700

    eidt a spot completed

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -180,6 +180,40 @@ router.get(
 //edit a spot
 
 router.put ('/:spotId', requireAuth, async(req, res)=> {
+    const {address, city, state, country, lat, lng, name, description, price} = req.body;
+    if(!address || !city || !state || !country || !lat || !lng || !name || !description || !price) {
+        res.status(400);
+        return res.json({
+            "message": "Validation Error",
+            "statusCode": 400,
+            "errors": {
+                "address": "Street address is required",
+                "city": "City is required",
+                "state": "State is required",
+                "country": "Country is required",
+                "lat": "Latitude is not valid",
+                "lng": "Longitude is not valid",
+                "name": "Name must be less than 50 characters",
+                "description": "Description is required",
+                "price": "Price per day is required"
+            
+        }
+    })    
     
+    }
+    const spot = await Spot.findByPk(req.params.spotId);
+    console.log(spot)
+    if(spot){
+        const updatedSpot = await spot.update({
+            address, city, state, country, lat, lng, name, description, price
+        })
+        return res.json(updatedSpot)
+    } else{
+        res.status(404)
+        return res.json({
+            "message": "Spot couldn't be found",
+            "statusCode": 404
+        })
+    }
 })
 module.exports = router;
