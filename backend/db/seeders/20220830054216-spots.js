e39c0f74d152ecf73e6f6fa0c3d335d9e6786c99
nachen98 <nachen98@gmail.com>
2022-09-25 20:02:29 -0700

    fixed bugs for seeding data

--- backend/db/seeders/20220830054216-spots.js
+++ backend/db/seeders/20220830054216-spots.js
@@ -21,7 +21,7 @@ module.exports = {
         "lat": 56.3333331,
         "lng": -3.0000001,
         "name": "Kinnettles Mansion",
-        "description": "The Kinnettles Mansion hosts eight large bedrooms/ suites, each of which has its own individual character and decor from the original interiors when the property was first built albeit tastefully restored over the years. This Stunning Property is around 5 minutes from St Andrews. With stunning gardens to relax and unwind, this is a bespoke property, the ultimate place to be while visiting St Andrews and the surrounding areas.",
+        "description": "The Kinnettles Mansion hosts eight large bedrooms/ suites, each of which has its own individual character and decor from the original interiors when the property was first built albeit tastefully restored over the years.",
         "price": "630"
     },
     {
@@ -33,7 +33,7 @@ module.exports = {
         "lat": 34.2354166,
         "lng": -117.1792338,
         "name": "Castle in the Forest",
-        "description": "Nestled in a deep canyon nearby serene Lake Arrowhead \u2013 you will find Southern California\u2019s secret gem: the Castle In the Forest. Surrounded by towering pine and dogwood trees, this secluded 10,000 square foot chateau provides an unforgettable intimate setting for your stay.",
+        "description": "Nestled in a deep canyon nearby serene Lake Arrowhead \u2013 you will find Southern California\u2019s secret gem: the Castle In the Forest.",
         "price": "3450"
     },
     {
@@ -45,7 +45,7 @@ module.exports = {
         "lat": 34.0696501,
         "lng": -118.3963062,
         "name": "Chateau de Laurel",
-        "description": "Cancel before 4:00 PM on Oct 1 and only get a refund of the hospitality fee.",
+        "description": "Cancel before 4:00 PM on Oct 1 and only get a refund of the hospitality fee..",
         "price": "15840"
     },
     {
@@ -57,7 +57,7 @@ module.exports = {
         "lat": 34.2585615,
         "lng": -117.18300401517857,
         "name": "Spectacular Lake Front Retreat",
-        "description": "LUXURY LAKE FRONT historic property- newly remodeled with updated features! Relax in the Restoration Hardware furniture as you enjoy the wonderful lake views from nearly every room in the house. Features organic cotton mattresses (including 4 Cal Kings) & ultra-high end custom Pom Pom bedding throughout. This secluded beautifully landscaped property with serene waterfall & pond offers oversized parking accommodations with easy access to the home.",
+        "description": "LUXURY LAKE FRONT historic property- newly remodeled with updated features! Relax in the Restoration Hardware furniture as you enjoy the wonderful lake views from nearly every room in the house.",
         "price": "2000"
     },
     {
@@ -69,7 +69,7 @@ module.exports = {
         "lat": 33.757805,
         "lng": -116.4354884,
         "name": "'Chateau Dumont' 18,000 sqft, 15 BR/13 BA Resort Compound",
-        "description": "The Chateau Dumont (formerly known as the Solomon Estate) is a spectacular 5 acre paradise that sleeps 30, with 15 bedrooms, 13 bathrooms and 18,000 square feet in the heart of Rancho Mirage. One of a kind, and privately gated, its lovely grounds feature a Vegas-style pool and spa, professional lighted tennis court, beach volleyball court and ping pong. The picturesque grounds have swaying palms, lush grass, waterfalls, lakes with Koi fish and turtles, as well as kayaks available for use on the lake. Incredible outdoor living and dining, with plenty of areas to kick back with your group. Outdoor kitchen, dining, misters, fireplace and lounge chairs.",
+        "description": "The Chateau Dumont (formerly known as the Solomon Estate) is a spectacular 5 acre paradise that sleeps 30, with 15 bedrooms, 13 bathrooms and 18,000 square feet in the heart of Rancho Mirage.",
         "price": "3928"
     },
     {
@@ -81,7 +81,7 @@ module.exports = {
         "lat": 39.4829124,
         "lng": -106.0465486,
         "name": "Luxury w/Mountain Views + Hot Tub/Elevator + Ski in/Ski out - Epic Retreat",
-        "description": "With meticulous attention to detail and designed to deliver an epic experience, you will lack for nothing while vacationing at Epic Retreat, a ski-in/ski-out designer home.",
+        "description": "With meticulous attention to detail and designed to deliver an epic experience, you will lack for nothing while vacationing at Epic Retreat, a ski-in/ski-out designer home..",
         "price": "2225"
     },
     {
@@ -93,7 +93,7 @@ module.exports = {
         "lat": 56.166395,
         "lng": -3.7513968,
         "name": "DOLLARBEG CASTLE - The Tower - luxury 3 bed rental",
-        "description": "DOLLARBEG CASTLE is a unique castle holiday location in Scotland. This luxury apartment has 3 bedrooms, a cinema room & tower, with private rooftop terrace and breathtaking panoramic views of the surrounding Clackmannanshire countryside, including the Ochil Hills.",
+        "description": "DOLLARBEG CASTLE is a unique castle holiday location in Scotland.",
         "price": "373"
     },
     {
@@ -105,7 +105,7 @@ module.exports = {
         "lat": 56.0701208,
         "lng": -3.9087351,
         "name": "Duchray Castle - 16th Century Scottish Castle",
-        "description": "2.5 miles from Aberfoyle, nestled in the heart of the Queen Elizabeth Forest this 500 year old tower house with connections to Rob Roy has been fully renovated. It now offers comfortable accommodation with en suite bathrooms to each bedroom. With renewable biomass heating system Duchray Castle is the perfect cosy get away! ",
+        "description": "2.5 miles from Aberfoyle, nestled in the heart of the Queen Elizabeth Forest this 500 year old tower house with connections to Rob Roy has been fully renovated. It now offers comfortable accommodation with en suite bathrooms to each bedroom.",
         "price": "489"
     },
     {
@@ -117,19 +117,19 @@ module.exports = {
         "lat": 47.8203106,
         "lng": -121.5543876,
         "name": "The Treeframe Cabin",
-        "description": "Outrageously beautiful modern treehouse aframe cabin perched 13ft off the ground between 4 evergreen trees. Brand new with luxurious finishes, a two person hot tub, full bathroom, fireplace, giant skylights, and a king bed sleeping loft. We proudly donate portions of guest proceeds to The Sierra Club, Forterra (saved Lake Serene Trail), WTA, and The Tulalip Foundation.",
+        "description": "Outrageously beautiful modern treehouse aframe cabin perched 13ft off the ground between 4 evergreen trees. Brand new with luxurious finishes, a two person hot tub, full bathroom, fireplace, giant skylights, and a king bed sleeping loft.",
         "price": "543"
     },
     {
         "ownerId": 5,
-        "address": "Jema\u00e2 El Fna, Derb Boukili, Mouassine, Medina \u2d4e\u2d37\u2d49\u2d4f\u2d30 \u0627\u0644\u0645\u062f\u064a\u0646\u0629, Marrakech \u2d4e\u2d55\u2d55\u2d30\u2d3d\u2d6f\u2d5b \u0645\u0631\u0627\u0643\u0634, Pachalik de Marrakech, Pr\u00e9fecture de Marrakech \u0639\u0645\u0627\u0644\u0629 \u0645\u0631\u0627\u0643\u0634, Marrakech-Safi \u2d4e\u2d55\u2d55\u2d30\u2d3d\u2d5b-\u2d30\u2d59\u2d3c\u2d49 \u0645\u0631\u0627\u0643\u0634-\u0623\u0633\u0641\u064a, 40034, Maroc / \u2d4d\u2d4e\u2d56\u2d54\u2d49\u2d31 / \u0627\u0644\u0645\u063a\u0631\u0628",
+        "address": "Jema\u00e2 El Fna, Derb Boukili, Mouassine, Medina",
         "city": "Marrakech",
         "state": "Marrakech-Safi",
         "country": "Morocco",
         "lat": 31.6258257,
         "lng": -7.9891608,
         "name": "Charming Riad/Pool /Spa/Restaurant in Marrakech",
-        "description": "Some info has been automatically translated.",
+        "description": "Some info has been automatically translated..",
         "price": "143"
     },
     {
@@ -141,7 +141,7 @@ module.exports = {
         "lat": 37.6774238,
         "lng": -113.061827,
         "name": "Hobbit Cottage",
-        "description": "Tucked away in our peaceful garden, this modern Hobbit Cottage will delight you! Although it's not the Shire of Middle Earth from LOTR, it's our little piece of paradise. We are located near Bryce Canyon, Brian Head and Zion National Park, Kannarraville Falls. Custom built by Chris and I for those who love adventure, hiking, snow boarding. Make sure to catch the world-famous Shakespeare festival. Cedar City is known as \"Festival City\" for a good reason!",
+        "description": "Tucked away in our peaceful garden, this modern Hobbit Cottage will delight you! Although it's not the Shire of Middle Earth from LOTR, it's our little piece of paradise.",
         "price": "58"
     },
     {
@@ -153,7 +153,7 @@ module.exports = {
         "lat": 46.247171,
         "lng": -89.729323,
         "name": "Dog-friendly home on private island w/boat landing",
-        "description": "Take advantage of this once in a lifetime adventure in the Northwoods on your own private island on Katinka Lake!",
+        "description": "Take advantage of this once in a lifetime adventure in the Northwoods on your own private island on Katinka Lake!.",
         "price": "1285"
     },
     {
@@ -165,7 +165,7 @@ module.exports = {
         "lat": 34.0234337,
         "lng": -84.6154897,
         "name": "Treasure Hunt Tree House in Metro Atlanta",
-        "description": "Welcome to Treasure Hunt Tree House, located in Kennesaw GA (about 20 min drive from Atlanta)",
+        "description": "Welcome to Treasure Hunt Tree House, located in Kennesaw GA (about 20 min drive from Atlanta).",
         "price": "175"
     },
     {
@@ -177,7 +177,7 @@ module.exports = {
         "lat": 34.035591,
         "lng": -118.689423,
         "name": "The Carbon",
-        "description": "Cancel before 4:00 PM on Oct 1 and only get a refund of the hospitality fee.",
+        "description": "Cancel before 4:00 PM on Oct 1 and only get a refund of the hospitality fee..",
         "price": "3551"
     },
     {
@@ -189,7 +189,7 @@ module.exports = {
         "lat": 20.742311,
         "lng": -105.3918464,
         "name": "Paraiso Real del Mar",
-        "description": "Cancel before 3:00 PM on Oct 1 and only get a refund of the hospitality fee.",
+        "description": "Cancel before 3:00 PM on Oct 1 and only get a refund of the hospitality fee..",
         "price": "8000"
     },
     {
@@ -201,7 +201,7 @@ module.exports = {
         "lat": 36.9092773,
         "lng": -121.7529071,
         "name": "Redwood Retreat",
-        "description": "Enjoy Redwoods while relaxing on large deck or inside by indoor woodstove (woodstove fee $50). Our rustic, remote cabin, surrounded by Redwoods, is perfect for water conserving, quiet guests who want to get away from it all to relax & unwind. Please be respectful to neighbors & follow house rules/no trespassing. Limited Cell & Satellite WIFI. Self service wood burning heated cedar hot tub additional fee $200 & advance notice.",
+        "description": "Enjoy Redwoods while relaxing on large deck or inside by indoor woodstove (woodstove fee $50).",
         "price": "231"
     },
     {
@@ -213,7 +213,7 @@ module.exports = {
         "lat": 42.9156191,
         "lng": 12.1444745,
         "name": "Old Castle Olive Mill",
-        "description": "Charming and completely restored 13th century castle olive mill, transformed into a luxurious country house, located just outside the ring wall of Castello di Montegiove.",
+        "description": "Charming and completely restored 13th century castle olive mill, transformed into a luxurious country house, located just outside the ring wall of Castello di Montegiove..",
         "price": "280"
     },
     {
@@ -225,7 +225,7 @@ module.exports = {
         "lat": 38.2971367,
         "lng": -122.2855293,
         "name": "White House Napa Valley Inn",
-        "description": "Great hospitality, great downtown location & very conscious of following covid precautions. Highly recommend!",
+        "description": "Great hospitality, great downtown location & very conscious of following covid precautions. Highly recommend!.",
         "price": "12055"
     },
     {
@@ -237,7 +237,7 @@ module.exports = {
         "lat": 34.0536909,
         "lng": -118.242766,
         "name": "The Hideaway",
-        "description": "Cancel before 4:00 PM on Oct 1 and only get a refund of the hospitality fee.",
+        "description": "Cancel before 4:00 PM on Oct 1 and only get a refund of the hospitality fee..",
         "price": "3560"
     },
     {
@@ -249,7 +249,7 @@ module.exports = {
         "lat": 34.0696501,
         "lng": -118.3963062,
         "name": "Chateau de Laurel",
-        "description": "Cancel before 4:00 PM on Oct 1 and only get a refund of the hospitality fee.",
+        "description": "Cancel before 4:00 PM on Oct 1 and only get a refund of the hospitality fee..",
         "price": "15840"
     }
 ])
